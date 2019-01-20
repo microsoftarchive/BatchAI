f148e326ed5891298242b9afda9d234ec12edf18
Li Li <li.li.signal@gmail.com>
2019-01-19 22:28:08 -0800

    Update CIFAR-10_data_prepare.sh

--- recipes/CNTK/CNTK-GPU-Python-Distributed/CIFAR-10_data_prepare.sh
+++ recipes/CNTK/CNTK-GPU-Python-Distributed/CIFAR-10_data_prepare.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 if [ ! -z $AZ_BATCHAI_JOB_TEMP ];then
     cd $AZ_BATCHAI_JOB_TEMP
-    wget 'https://batchaisamples.blob.core.windows.net/samples/CIFAR-10_dataset.tar?st=2017-09-29T18%3A29%3A00Z&se=2099-12-31T08%3A00%3A00Z&sp=rl&sv=2016-05-31&sr=b&sig=nFXsAp0Eq%2BoS5%2BKAEPnfyEGlCkBcKIadDvCPA%2BcX6lU%3D' -k -O 'CIFAR-10_dataset.tar'
+    wget 'https://batchaisamples.blob.core.windows.net/samples/CIFAR-10_dataset.tar?st=2019-01-03T22%3A05%3A00Z&se=2099-12-31T20%3A00%3A00Z&sp=rl&sv=2017-04-17&sr=c&sig=5UxAYC%2BrBAHdYmDLvHACYl3ZDPmmO51QhNHBlVuO9vg%3D' -k -O 'CIFAR-10_dataset.tar'
     echo "untar CIFAR-10 dataset........."
     tar -xf CIFAR-10_dataset.tar
     echo "done"
